134f63bfe8a8a407ce3a3cf662d8e0db8fb604fe
GabrielLimaBzr <gabriel18lb@outlook.com>
2023-08-09 19:15:38 -0300

    scroll reveal

--- src/app/components/home/home.component.ts
+++ src/app/components/home/home.component.ts
@@ -1,10 +1,61 @@
-import { Component } from '@angular/core';
+import { Component, ElementRef, HostListener, OnInit } from '@angular/core';
 
 @Component({
   selector: 'app-home',
   templateUrl: './home.component.html',
   styleUrls: ['./home.component.scss']
 })
-export class HomeComponent {
+export class HomeComponent implements OnInit{
+
+  revealPoint = 150;
+
+
+  constructor(private elementRef: ElementRef) {}
+
+  ngOnInit(): void {
+    this.checkAndAdjustReveal();
+  }
+
+  @HostListener('window:resize', ['$event'])
+  onWindowResize() {
+    this.checkAndAdjustReveal(); // Ajuste o reveal ao redimensionar a janela
+  }
+
+  @HostListener('window:scroll', ['$event'])
+  onWindowScroll() {
+    this.reveal();
+  }
+
+  reveal() {
+    var rvls = this.elementRef.nativeElement.querySelectorAll('.reveal');
+
+    for (var i = 0; i < rvls.length; i++) {
+      var windowTam = window.innerHeight;
+      var revealTop = rvls[i].getBoundingClientRect().top;
+
+      if (revealTop < windowTam - this.revealPoint) {
+        rvls[i].classList.add('active');
+      } else {
+        rvls[i].classList.remove('active');
+      }
+    }
+  }
+
+  checkAndAdjustReveal() {
+    if (window.innerWidth <= 767) { // Defina o valor de largura apropriado para dispositivos móveis
+      this.revealPoint = 50
+      var elementoSobre = this.elementRef.nativeElement.querySelector('#sobre');
+      if (elementoSobre) {
+        elementoSobre.classList.remove('reveal');
+      }
+    } else {
+      this.revealPoint = 150
+      var elementoSobre = this.elementRef.nativeElement.querySelector('#sobre');
+      if (elementoSobre) {
+        elementoSobre.classList.add('reveal');
+      }
+    }
+  }
 
 }
+
