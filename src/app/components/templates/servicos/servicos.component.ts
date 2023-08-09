575528ef3da4ea9b6b78af2bd5e8b2101459a50c
GabrielLimaBzr <gabriel18lb@outlook.com>
2023-08-09 15:18:23 -0300

    ajustes de responsividade

--- src/app/components/templates/servicos/servicos.component.ts
+++ src/app/components/templates/servicos/servicos.component.ts
@@ -7,4 +7,26 @@ import { Component } from '@angular/core';
 })
 export class ServicosComponent {
 
+  cardsData = [
+    {
+      title: 'Rebocadores',
+      srcImg: 'assets/barco.png',
+      text: 'Rebocadores são embarcações projetadas para manobrar e rebocar barcos e navios, garantindo a segurança da navegação em situações delicadas, como entrada e saída em portos e canais.'
+    },
+    {
+      title: 'Mergulhadores',
+      srcImg: 'assets/mergulho.png',
+      text: 'A escafandaria é uma atividade que envolve mergulho com equipamentos especiais para realizar inspeções, reparos e manutenções em estruturas subaquáticas, como cascos de navios, plataformas de petróleo e estruturas de pontes e barragens.'
+    },
+    {
+      title: 'Caminhão Munck',
+      srcImg: 'assets/caminhao.png',
+      text: 'Quase todos os itens de carga são transportados em algum momento de sua viagem ao redor do mundo, para isso contamos com uma frota de transporte terrestre igualmente tecnológica.'
+    },
+    {
+      title: 'Segurança e Qualidade',
+      srcImg: 'assets/selo.png',
+      text: 'Oferecemos nossos serviços de forma segura e com a qualidade que nossos clientes merecem. Optamos por embarcações novas e revisadas, afim de prestar os melhores serviços.'
+    }
+  ];
 }
